bfbcf10a77fd7d57cae35308f45c0cbcf5e54e33
HliasGit <102186706+HliasGit@users.noreply.github.com>
2022-10-07 18:06:15 +0200

    initial commit

---
+++ src/main/java/Group2/BWEProject/BweProjectApplication.java
@@ -0,0 +1,13 @@
+package Group2.BWEProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BweProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BweProjectApplication.class, args);
+	}
+
+}
