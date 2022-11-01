bba6c472fd014ec62eab952bdf2a2ada54e29ad6
baskaklimek <baska.klimekova@gmail.com>
2022-11-01 08:32:41 +0100

    persist data into h2 database

--- src/main/java/Group2/BWEProject/BweProjectApplication.java
+++ src/main/java/Group2/BWEProject/BweProjectApplication.java
@@ -1,13 +1,28 @@
 package Group2.BWEProject;
 
+import Group2.BWEProject.dao.AuctionDao;
+import Group2.BWEProject.dao.AuctionDataAccessService;
+import Group2.BWEProject.model.Auction;
+import org.springframework.boot.ConfigurableBootstrapContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.time.LocalDate;
+import java.util.UUID;
 
 @SpringBootApplication
 public class BweProjectApplication {
 
 	public static void main(String[] args) {
+		ConfigurableApplicationContext configurableApplicationContext =
 		SpringApplication.run(BweProjectApplication.class, args);
+		AuctionDataAccessService auctionRepository = configurableApplicationContext.getBean(AuctionDataAccessService.class);
+		Auction auctionDbRecord = new Auction("ActionOne", LocalDate.now(),LocalDate.of(2022,11,30), UUID.randomUUID(),UUID.randomUUID(),true);
+
+		//persist into DB
+		auctionRepository.addAuction(auctionDbRecord);
 	}
+
 
 }
