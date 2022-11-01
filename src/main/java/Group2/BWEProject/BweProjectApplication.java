d43d961dc8180049798b2de66f47c6f2871a9733
baskaklimek <baska.klimekova@gmail.com>
2022-11-01 11:49:00 +0100

    resolve the conflicts

--- src/main/java/Group2/BWEProject/BweProjectApplication.java
+++ src/main/java/Group2/BWEProject/BweProjectApplication.java
@@ -1,8 +1,7 @@
 package Group2.BWEProject;
 
-import Group2.BWEProject.dao.ProductDao;
-import Group2.BWEProject.dao.ProductDataAccessService;
-import Group2.BWEProject.model.Product;
+import Group2.BWEProject.dao.AuctionDataAccessService;
+import Group2.BWEProject.model.Auction;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -14,54 +13,15 @@ import java.util.UUID;
 public class BweProjectApplication {
 
 	public static void main(String[] args) {
+		ConfigurableApplicationContext configurableApplicationContext =
 		SpringApplication.run(BweProjectApplication.class, args);
+		AuctionDataAccessService auctionRepository = configurableApplicationContext.getBean(AuctionDataAccessService.class);
+		Auction auctionDbRecord = new Auction("ActionOne", LocalDate.now(),LocalDate.of(2022,11,30), UUID.randomUUID(),UUID.randomUUID(),true);
+
+		//persist into DB
+		auctionRepository.addAuction(auctionDbRecord);
 	}
 
+
 }
-
-
-//package Group2.BWEProject;
-//
-//        import Group2.BWEProject.dao.ProductDao;
-//        import Group2.BWEProject.dao.ProductDataAccessService;
-//        import Group2.BWEProject.model.Product;
-//        import org.springframework.boot.SpringApplication;
-//        import org.springframework.boot.autoconfigure.SpringBootApplication;
-//        import org.springframework.context.ConfigurableApplicationContext;
-//
-//        import java.time.LocalDate;
-//        import java.util.UUID;
-//
-//@SpringBootApplication
-//public class BweProjectApplication {
-//
-//package Group2.BWEProject;
-//
-//import Group2.BWEProject.dao.ProductDao;
-//import Group2.BWEProject.dao.ProductDataAccessService;
-//import Group2.BWEProject.model.Product;
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.context.ConfigurableApplicationContext;
-//
-//import java.time.LocalDate;
-//import java.util.UUID;
-//
-//    @SpringBootApplication
-//    public class BweProjectApplication {
-//
-//        public static void main(String[] args) {
-//            ConfigurableApplicationContext configurableApplicationContext =
-//                    SpringApplication.run(Group2.BWEProject.BweProjectApplication.class, args);
-//            Product productRepository = configurableApplicationContext.getBean(ProductDataAccessService.class);
-//            ProductDao productRepository = configurableApplicationContext.getBean(ProductDao.class);
-//            ///TODO
-//            Product productDbRecord = new Product("ActionOne", LocalDate.now(), LocalDate.of(2022, 11, 30), UUID.randomUUID(), UUID.randomUUID(), true);
-//
-//            //persist into DB
-//            productRepository.insertProduct(productDbRecord);
-//            productRepository.save(productDbRecord);
-//        }
-//    }
-//}
 
