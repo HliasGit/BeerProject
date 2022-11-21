93e9e6432095e24bb7d261663b6a1e8cbb749f3a
Eyal <eyal.davidi@edu.turkuamk.fi>
2022-11-21 16:39:07 +0100

    Adding data validations to Product class

--- src/main/java/Group2/BWEProject/model/Product.java
+++ src/main/java/Group2/BWEProject/model/Product.java
@@ -1,8 +1,5 @@
 package Group2.BWEProject.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import org.springframework.lang.NonNull;
-
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
@@ -14,66 +11,29 @@ import java.util.UUID;
 public class Product{
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private final UUID id;
+    private UUID id;
 
-    @NotBlank//((message = "username is mandatory"))
+    @NotBlank (message = "username is mandatory")
     @Size(min = 2, max = 15)
     private String user;
 
-    @NonNull //((message = "Name is mandatory"))
-//    private final String name;
+    @NotBlank (message = "Name is mandatory")
+    @Size(min = 2, max = 15)
     private String name;
 
-    @NonNull //((message = "Description is mandatory"))
-    private final String description;
-    private final String image; ///which type?
-    @NonNull //((message = "Minimum price is mandatory"))
+    @NotBlank (message = "Description is mandatory")
+    @Size(min = 2, max = 80)
+    private String description;
+    private String image; ///which type?
+    @NotBlank (message = "Minimum price is mandatory")
     private Double minPrice;
+    @NotBlank (message = "Expected price is mandatory")
     private Double expectedPrice; //bigDecimal?
-    @NonNull //((message = "Expected delivery time is mandatory"))
+    @NotBlank(message = "Expected delivery time is mandatory")
     private Integer expectedDeliveryTimeInDays;
 //    private Boolean onAuction;  //Auction auction
-
-    public UUID getId() {return id;}
-    @NonNull
-    public String getUser() {return user;}
-    @NonNull
-    public String getName() {
-        return name;
-    }
-    @NonNull
-    public String getDescription() {
-        return description;
-    }
-    public String getImage() {
-        return image;
-    }
-    @NonNull
-    public Double getMinPrice() {
-        return minPrice;
-    }
-
-    public Double getExpectedPrice() {
-        return expectedPrice;
-    }
-    @NonNull
-    public Integer getExpectedDeliveryTimeInDays() {
-        return expectedDeliveryTimeInDays;
-    }
-//    public Boolean getOnAuction() {
-//        return onAuction;
-//    }
-
-    public Product(@JsonProperty("id") UUID id,
-                @JsonProperty("user")String user,
-                @JsonProperty("name")String name,
-                @JsonProperty("description")String description,
-                @JsonProperty("image")String image,
-                @JsonProperty("min price")Double minPrice,
-                @JsonProperty("expected price")Double expectedPrice,
-                @JsonProperty("expected delivery time in days")Integer expectedDeliveryTimeInDays) {
-
-        this.id = id;
+    public Product(String user, String name, String description, String image, Double minPrice,
+                   Double expectedPrice, Integer expectedDeliveryTimeInDays) {
         this.user= user;
         this.name = name;
         this.description = description;
@@ -82,6 +42,38 @@ public class Product{
         this.expectedPrice = expectedPrice;
         this.expectedDeliveryTimeInDays = expectedDeliveryTimeInDays;
 //        this.onAuction = onAuction;
+    }
+    public Product(){
+    }
+    public UUID getId(){
+        return id;
+    }
+    @NotBlank
+    public String getUser() {
+        return user;
+    }
+    @NotBlank
+    public String getName() {
+        return name;
+    }
+    @NotBlank
+    public String getDescription() {
+        return description;
+    }
+    public String getImage() {
+        return image;
+    }
+    @NotBlank
+    public Double getMinPrice() {
+        return minPrice;
+    }
+    @NotBlank
+    public Double getExpectedPrice() {
+        return expectedPrice;
+    }
+    @NotBlank
+    public Integer getExpectedDeliveryTimeInDays() {
+        return expectedDeliveryTimeInDays;
     }
 }
 
