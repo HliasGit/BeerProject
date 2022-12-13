da8d2c3c2c5e4bb1c47933439320f082cc779997
Elia Vaglietti <vagliettielia@gmail.com>
2022-12-13 16:33:10 +0100

    User selectAll test

--- src/test/java/Group2/BWEProject/service/UserServiceTest.java
+++ src/test/java/Group2/BWEProject/service/UserServiceTest.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -18,8 +20,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class UserServiceTest {
 
     UUID random = UUID.randomUUID();
-    Optional<User> user = Optional.of(new User("Ciao", "Vaffanculo", "ciaostrono@gmail.com", "password", false));
-
+    Optional<User> user1 = Optional.of(new User("Ciao", "Vaffanculo", "ciaostrono@gmail.com", "password", false));
+    Optional<User> user2 = Optional.of(new User("Cane", "Stuffs", "afasano@gmail.com", "password", false));
+    List<User> userList = new ArrayList<>();
 
     @Autowired
     private UserService userService;
@@ -29,8 +32,12 @@ class UserServiceTest {
 
     @BeforeEach
     void setup(){
-        Mockito.when(userRepository.findById(random)).thenReturn(user);
-        Mockito.when(userRepository.save(user.get())).thenReturn(user.get());
+        userList.add(user1.get());
+        userList.add(user2.get());
+
+        Mockito.when(userRepository.findById(random)).thenReturn(user1);
+        Mockito.when(userRepository.save(user1.get())).thenReturn(user1.get());
+        Mockito.when(userRepository.findAll()).thenReturn(userList);
     }
 
     @Test
@@ -43,7 +50,14 @@ class UserServiceTest {
     @Test
     public void testAddUser(){
         String userName = "Ciao";
-        User userFound = userService.addUser(user.get());
+        User userFound = userService.addUser(user1.get());
         assertEquals(userName, userFound.getFirstName());
+    }
+
+    @Test
+    public void testSelectAllUsers(){
+        String userName = "Cane";
+        User userFound = userService.selectAllUsers().get(1);
+        assertEquals(userFound.getFirstName(), userName);
     }
 }
