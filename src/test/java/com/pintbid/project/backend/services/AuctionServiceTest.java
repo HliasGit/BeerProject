d69804e46018fc69b6ac31c5c48e59f5f3ba79e9
Elia Vaglietti <vagliettielia@gmail.com>
2023-01-10 12:30:03 +0100

    auction test part 2

--- src/test/java/com/pintbid/project/backend/services/AuctionServiceTest.java
+++ src/test/java/com/pintbid/project/backend/services/AuctionServiceTest.java
@@ -10,6 +10,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
@@ -18,6 +22,9 @@ import static org.mockito.Mockito.when;
 class AuctionServiceTest {
 
     Auction auction1 = new Auction("test", "description", 12.2, 34.2, LocalDate.of(2012, 11, 13), LocalDate.of(2013, 11, 13), 12, 14, EAuctionStatus.ACTIVE, 34, "si");
+    Auction auction2 = new Auction("test2", "description2", 12.2, 34.2, LocalDate.of(2012, 11, 13), LocalDate.of(2013, 11, 13), 12, 14, EAuctionStatus.LOCKED, 35, "si");
+    List<Auction> list = new ArrayList<>();
+    List<Auction> listUnl = new ArrayList<>();
 
     @Autowired
     private AuctionService auctionService;
@@ -27,7 +34,12 @@ class AuctionServiceTest {
 
     @BeforeEach
     void setup(){
+        list.add(auction1);
+        list.add(auction2);
+        listUnl.add(auction2);
         when(auctionRepository.save(auction1)).thenReturn(auction1);
+        when(auctionRepository.findAll()).thenReturn(list);
+        when(auctionRepository.findById(auction1.getId())).thenReturn(Optional.ofNullable(auction1));
     }
 
     @Test
@@ -38,14 +50,23 @@ class AuctionServiceTest {
 
     @Test
     void selectAllAuctions() {
+        Integer id = auction1.getId();
+        Auction test = auctionService.selectAllAuctions().get(0);
+        assertEquals(id, test.getId());
     }
 
     @Test
     void selectAuctionById() {
+        Integer id = auction1.getId();
+        Optional<Auction> auctionFound = auctionService.selectAuctionById(auction1.getId());
+        assertEquals(id, auctionFound.get().getId());
     }
 
     @Test
     void updateAuctionById() {
+        Integer id = auction1.getId();
+        Auction auc = auctionService.updateAuctionById(auction2.getId(), auction1);
+        assertEquals(id, auc.getId());
     }
 
     @Test
@@ -54,5 +75,7 @@ class AuctionServiceTest {
 
     @Test
     void getUnlockedAutions() {
+        List<Auction> listOfAuctions = (List<Auction>) auctionService.getUnlockedAutions();
+        assertEquals(listUnl, listOfAuctions);
     }
 }
