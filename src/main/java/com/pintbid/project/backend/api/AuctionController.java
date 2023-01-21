a79d251dc095d63150f0f217ccd23bd1d415d311
baskaklimek <baska.klimekova@gmail.com>
2023-01-21 11:50:29 +0100

    adding new auction andpoints

--- src/main/java/com/pintbid/project/backend/api/AuctionController.java
+++ src/main/java/com/pintbid/project/backend/api/AuctionController.java
@@ -2,15 +2,19 @@ package com.pintbid.project.backend.api;
 
 import com.pintbid.project.backend.models.Auction;
 import com.pintbid.project.backend.services.AuctionService;
+import com.pintbid.project.backend.utils.EAuctionCategory;
+import com.pintbid.project.backend.utils.EAuctionStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RequestMapping("api/auctions")
 @RestController
@@ -105,25 +109,51 @@ public class AuctionController {
     }
 
     @GetMapping(path = "/category/{category}")
-    public ResponseEntity<Auction> selectAuctionCategory(@PathVariable("category") Integer id) {
+    public ResponseEntity<List<Auction>>selectAuctionByCategory(@PathVariable("category") EAuctionCategory category) {
 
-        Optional<Auction> auctionData = auctionService.selectAuctionById(id);
-        if (auctionData.isPresent()) {
-            return new ResponseEntity<>(auctionData.get(), HttpStatus.OK);
+        List<Auction> auctions = new ArrayList<Auction>();
+        auctionService.selectAllAuctions().forEach(auctions::add);
+
+        List<Auction> auctionByCategory = auctions.stream()
+                .filter(auction -> auction.getCategory().equals(category))
+                .collect(Collectors.toList());
+        ;
+        if (!auctionByCategory.isEmpty()) {
+            return new ResponseEntity<>(auctionByCategory, HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+    }
 
-        //  return auctionData.map(auction -> new ResponseEntity<>(auction, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    @GetMapping(path = "/status/{status}")
+    public ResponseEntity<List<Auction>>selectAuctionByStatus(@PathVariable("status") EAuctionStatus status) {
 
+        List<Auction> auctions = new ArrayList<Auction>();
+        auctionService.selectAllAuctions().forEach(auctions::add);
+
+        List<Auction> auctionByStatus = auctions.stream()
+                .filter(auction -> auction.getEAuctionStatus().equals(status))
+                .collect(Collectors.toList());
+        ;
+        if (!auctionByStatus.isEmpty()) {
+            return new ResponseEntity<>(auctionByStatus, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @GetMapping(path = "/date/{endDate}")
-    public ResponseEntity<Auction> selectAuctionByEndDate(@PathVariable("endDate") Integer id) {
+    public ResponseEntity<List<Auction>> selectByEndDate(@PathVariable("endDate") LocalDate endDate) {
 
-        Optional<Auction> auctionData = auctionService.selectAuctionById(id);
-        if (auctionData.isPresent()) {
-            return new ResponseEntity<>(auctionData.get(), HttpStatus.OK);
+        List<Auction> auctions = new ArrayList<Auction>();
+        auctionService.selectAllAuctions().forEach(auctions::add);
+
+        List<Auction> auctionByEndDate = auctions.stream()
+                .filter(auction -> auction.getEndDate().equals(endDate))
+                .collect(Collectors.toList());
+        ;
+        if (!auctionByEndDate.isEmpty()) {
+            return new ResponseEntity<>(auctionByEndDate, HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
